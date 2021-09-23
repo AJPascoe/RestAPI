36589e0a577127cdbef4f5eb887aed1bacc6918b
AJPascoe <pascoe312@gmail.com>
2021-09-23 14:30:12 +0100

    connected to server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,13 @@
 require("./db/connection");
 const express = require("express");
+const app = express();
+const userRouter = require("./routes");
+
+app.use(express.json());
+
+
+app.use(userRouter);
+
+app.listen(5000, ()=>{
+    console.log("Listening on port 5000")
+});
