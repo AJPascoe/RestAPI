eccbbbeb2e0ed614253f4f1127a26096785cdb70
AJPascoe <pascoe312@gmail.com>
2021-09-23 16:09:22 +0100

    added post routes for create and read operators

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 const {Router} = require("express");
+const { addUser, findUser } = require("../controllers");
 const userRouter = Router();
+
+userRouter.post("/user", addUser);
+userRouter.post("/user", findUser);
 
 
 module.exports = userRouter;
