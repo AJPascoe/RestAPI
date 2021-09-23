c544cc4dba548b6dcb78152220f3b32156d3dce9
AJPascoe <pascoe312@gmail.com>
2021-09-23 15:53:17 +0100

    setting up model for user

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require("./db/connection");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const userRouter = require("./routes");
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use(userRouter);
